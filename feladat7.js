c5232bba06d62d3b3b582414273bdc13cd55da52
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2024-09-30 18:32:13 +0200

    Delete feladat7.js

--- feladat7.js
+++
@@ -1,9 +0,0 @@
-function Factorial() {
-
-    let number  = parseInt(document.getElementById("number").value);
-    let factorial = 1;
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-    }
-    document.getElementById("szam").innerText = `${number} faktorialisa ${factorial}.`;
-}
