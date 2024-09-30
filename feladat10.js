b63daf747d41e52d10997fefce76424a0b2ebada
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2024-09-30 18:29:37 +0200

    Delete feladat10.js

--- feladat10.js
+++
@@ -1,8 +0,0 @@
-let string = 'apple';
-let outputDiv = document.getElementById('output');
-
-for (let i = 0; i < string.length; i++) {
-    let letter = document.createElement('p');
-    letter.textContent = string[i];
-    outputDiv.appendChild(letter);
-}
