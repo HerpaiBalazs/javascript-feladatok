d594b7a15a80578be5422d9e4f55a3d4b7dccec1
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2024-09-30 18:30:56 +0200

    Delete feladat2.js

--- feladat2.js
+++
@@ -1,8 +0,0 @@
-let ora = 14;
-let perc = 34;
-let masodperc = 42;
-let osszmp = 24 * 60 * 60;
-
-let eltelt = (ora * 60 * 60) + (perc * 60) + masodperc;
-let hatralevo = osszmp - eltelt;
-document.getElementById('remaining').textContent = `Hátralevő másodpercek: ${hatralevo}`;
