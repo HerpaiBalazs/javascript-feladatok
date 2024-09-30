3d7e07239fcf30798c105c054decabad7bc0d7f8
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2024-09-30 18:29:06 +0200

    Delete feladat1.js

--- feladat1.js
+++
@@ -1,7 +0,0 @@
-let h = 10.4;
-let sz = 13.5;
-let m = 8.2;
-let felszin = 2 * (h * sz + sz * m + m * h);
-let terfogat = h * sz * m;
-document.getElementById('felszin').textContent = `Felszín: ${felszin}`;
-document.getElementById('terfogat').textContent = `Térfogat: ${terfogat}`;
